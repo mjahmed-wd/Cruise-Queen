7a7e094dadf0e3fcada501b71b13f9a30bfbaca1
Jubair Ahmed <mjahmed.wd@gmail.com>
2021-01-30 12:26:43 +0600

    comment added

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-function calculation(classIdName, additionOrder) {
-    if (additionOrder == true) {
-        document.getElementById(classIdName).value = readValue(classIdName) + 1
-    } else if (additionOrder == false && readValue(classIdName) > 0) {
-        document.getElementById(classIdName).value = readValue(classIdName) - 1
-    }
-    document.getElementById('subtotalAmount').innerText = subTotal()
-    document.getElementById('taxAmount').innerText = taxAmount()
-    document.getElementById('totalCost').innerText = totalCost()
-}
-
+// Reading the value from input field
 function readValue(classIdName) {
     var readValue = document.getElementById(classIdName)
-    var valueToNumber = 0
     if (isNaN(readValue.value) || (readValue.value) == "") {
-        return valueToNumber = 0
+        return 0
     } else {
-        return valueToNumber = parseInt(readValue.value)
+        return parseInt(readValue.value)
     }
 }
 
+// Calculating Subtotal
 function subTotal() {
     var firstClassCost = readValue('firstClassValue') * 150;
     var economyClassCost = readValue('economyClassValue') * 100;
@@ -26,36 +16,63 @@ function subTotal() {
     return subTotal
 }
 
+// Calculating Tax
 function taxAmount() {
     var taxAmount = parseInt(subTotal()) * .1;
     return taxAmount;
 }
 
+//Calculating Total Cost
 function totalCost() {
     var totalCost = parseInt(subTotal()) + parseInt(taxAmount());
     return totalCost;
 }
 
-function hideBookingForm() {
-    if (totalCost() == 0) {
-        document.getElementById('notification').innerText = 'Please select any seat to go further'
-    } else {
-        document.querySelector('.booking-form').style.display = 'none';
-        document.querySelector('.confirmation').style.display = 'block';
-        confirmData()
-    }
+//Print on Subtotal, Tax, Total Cost
+function printCost() {
+    document.getElementById('subtotalAmount').innerText = subTotal()
+    document.getElementById('taxAmount').innerText = taxAmount()
+    document.getElementById('totalCost').innerText = totalCost()
 }
 
+//Action after "+" or "-" button clicked
+function calculation(classIdName, additionOrder) {
+    if (additionOrder == true) {
+        document.getElementById(classIdName).value = readValue(classIdName) + 1
+    } else if (additionOrder == false && readValue(classIdName) > 0) {
+        document.getElementById(classIdName).value = readValue(classIdName) - 1
+    }
+    printCost()
+}
+
+//Hide a Section and show another one
+function toggleSection(hidingPartId, ShowingPartId) {
+    document.querySelector(hidingPartId).style.display = 'none';
+    document.querySelector(ShowingPartId).style.display = 'block';
+}
+
+//Show data on confirmation section
 function confirmData() {
     document.getElementById('confirmSubtotal').innerText = subTotal()
     document.getElementById('confirmTax').innerText = taxAmount()
     document.getElementById('confirmTotalCost').innerText = totalCost()
-
 }
 
+//Hide booking form and data Validation
+function hideBookingForm() {
+    if (totalCost() == 0) {
+        document.getElementById('notification').innerText = 'Please select any seat to go further'
+    } else {
+        toggleSection('.booking-form', '.confirmation')
+        confirmData()
+    }
+}
+
+//Hide Confirmation Section and book new one with resetting value to 0
 function hideConfirmation() {
-    document.querySelector('.booking-form').style.display = 'block';
-    document.querySelector('.confirmation').style.display = 'none';
-    document.getElementById('firstClassValue').value = 0
-    document.getElementById('economyClassValue').value = 0
+    toggleSection('.confirmation', '.booking-form')
+    document.getElementById('firstClassValue').value = ""
+    document.getElementById('economyClassValue').value = ""
+    document.getElementById('notification').innerText = ""
+    printCost()
 }
