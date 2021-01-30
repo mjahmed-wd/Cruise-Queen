2534e32bd1a1274eafab603ce8d75cca85f3348f
Jubair Ahmed <mjahmed.wd@gmail.com>
2021-01-30 12:39:13 +0600

    comment added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Reading the value from input field
 function readValue(classIdName) {
-    var readValue = document.getElementById(classIdName)
+    var readValue = document.getElementById(classIdName);
     if (isNaN(readValue.value) || (readValue.value) == "") {
-        return 0
+        return 0;
     } else {
-        return parseInt(readValue.value)
+        return parseInt(readValue.value);
     }
 }
 
@@ -13,7 +13,7 @@ function subTotal() {
     var firstClassCost = readValue('firstClassValue') * 150;
     var economyClassCost = readValue('economyClassValue') * 100;
     var subTotal = firstClassCost + economyClassCost;
-    return subTotal
+    return subTotal;
 }
 
 // Calculating Tax
@@ -30,17 +30,17 @@ function totalCost() {
 
 //Print on Subtotal, Tax, Total Cost
 function printCost() {
-    document.getElementById('subtotalAmount').innerText = subTotal()
-    document.getElementById('taxAmount').innerText = taxAmount()
-    document.getElementById('totalCost').innerText = totalCost()
+    document.getElementById('subtotalAmount').innerText = subTotal();
+    document.getElementById('taxAmount').innerText = taxAmount();
+    document.getElementById('totalCost').innerText = totalCost();
 }
 
 //Action after "+" or "-" button clicked
 function calculation(classIdName, additionOrder) {
     if (additionOrder == true) {
-        document.getElementById(classIdName).value = readValue(classIdName) + 1
+        document.getElementById(classIdName).value = readValue(classIdName) + 1;
     } else if (additionOrder == false && readValue(classIdName) > 0) {
-        document.getElementById(classIdName).value = readValue(classIdName) - 1
+        document.getElementById(classIdName).value = readValue(classIdName) - 1;
     }
     printCost()
 }
@@ -53,26 +53,26 @@ function toggleSection(hidingPartId, ShowingPartId) {
 
 //Show data on confirmation section
 function confirmData() {
-    document.getElementById('confirmSubtotal').innerText = subTotal()
-    document.getElementById('confirmTax').innerText = taxAmount()
-    document.getElementById('confirmTotalCost').innerText = totalCost()
+    document.getElementById('confirmSubtotal').innerText = subTotal();
+    document.getElementById('confirmTax').innerText = taxAmount();
+    document.getElementById('confirmTotalCost').innerText = totalCost();
 }
 
 //Hide booking form and data Validation
 function hideBookingForm() {
     if (totalCost() == 0) {
-        document.getElementById('notification').innerText = 'Please select any seat to go further'
+        document.getElementById('notification').innerText = 'Please select any seat to go further';
     } else {
-        toggleSection('.booking-form', '.confirmation')
-        confirmData()
+        toggleSection('.booking-form', '.confirmation');
+        confirmData();
     }
 }
 
 //Hide Confirmation Section and book new one with resetting value to 0
 function hideConfirmation() {
-    toggleSection('.confirmation', '.booking-form')
-    document.getElementById('firstClassValue').value = ""
-    document.getElementById('economyClassValue').value = ""
-    document.getElementById('notification').innerText = ""
-    printCost()
+    toggleSection('.confirmation', '.booking-form');
+    document.getElementById('firstClassValue').value = "";
+    document.getElementById('economyClassValue').value = "";
+    document.getElementById('notification').innerText = "";
+    printCost();
 }
